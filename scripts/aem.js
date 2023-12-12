b785ebda51d8afd0da74db5ed4f3280ed2161f4a
Vijay Kumar J <jalagari@adobe.com>
2023-12-12 22:59:40 +0530

    Including form in LCP

--- scripts/aem.js
+++ scripts/aem.js
@@ -657,7 +657,7 @@ async function loadFooter(footer) {
  * @param {Array} lcpBlocks Array of blocks
  */
 async function waitForLCP(lcpBlocks) {
-  const block = document.querySelector('.block');
+  const block = document.querySelector('.block.form'); // temporary fix for CLS
   const hasLCPBlock = block && lcpBlocks.includes(block.dataset.blockName);
   if (hasLCPBlock) await loadBlock(block);
 
